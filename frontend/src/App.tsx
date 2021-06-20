d949a9d4757ff8c878d6cf82339043d48111d633
Andrei Sold <sold_andrei@yahoo.com>
2021-06-20 13:18:16 +0300

    Configured Material UI theme and styled components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,41 @@
 import './App.css';
+import {
+  CssBaseline,
+  MuiThemeProvider,
+  NoSsr,
+  StylesProvider,
+  Typography,
+} from '@material-ui/core';
+import { theme } from './AppTheme';
+import styled, { ThemeProvider } from 'styled-components';
+
+const TestParagraph = styled(Typography)`
+  ${({ theme }) => `
+  color: ${theme.palette.secondary.main};
+  background-color: #fff;
+  box-shadow: 0 4px 6px rgba(50, 50, 93, 0.11), 0 1px 3px rgba(0, 0, 0, 0.08);
+  padding: 4px 10px;
+  font-size: 20px;
+  font-family: 'Roboto';
+`}
+`;
 
 const App = () => {
-  return <div>Running</div>;
+  return (
+    <NoSsr>
+      <StylesProvider injectFirst>
+        <MuiThemeProvider theme={theme}>
+          <ThemeProvider theme={theme}>
+            <CssBaseline>
+              <TestParagraph>
+                The Material UI theme and styled components are working
+              </TestParagraph>
+            </CssBaseline>
+          </ThemeProvider>
+        </MuiThemeProvider>
+      </StylesProvider>
+    </NoSsr>
+  );
 };
 
 export default App;
---
+++ frontend/src/AppTheme.tsx
@@ -0,0 +1,25 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+export const theme = createMuiTheme({
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 576,
+      md: 960,
+      lg: 1200,
+      xl: 1920,
+    },
+  },
+  palette: {
+    primary: {
+      main: '#0E1E40',
+    },
+    secondary: {
+      main: '#68C2E8',
+    },
+  },
+  typography: {
+    fontFamily: ['"Open Sans"', 'Roboto', 'Arial', 'sans-serif'].join(','),
+    fontSize: 16,
+  },
+});
