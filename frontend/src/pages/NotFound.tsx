d8fd3e277ee52d4fb3579be120243e6b01e5c9f4
Andrei Sold <sold_andrei@yahoo.com>
2021-06-22 17:27:45 +0300

    Added a missing margin top to the not found image

--- frontend/src/pages/NotFound.tsx
+++ frontend/src/pages/NotFound.tsx
@@ -3,7 +3,13 @@ import BackgroundImage from '../styled-components/BackgroundImage';
 
 const NotFound: React.FC = () => {
   return (
-    <Box display='flex' justifyContent='center' width='100%' minHeight='500px'>
+    <Box
+      display='flex'
+      justifyContent='center'
+      width='100%'
+      minHeight='500px'
+      marginTop='80px'
+    >
       <BackgroundImage path='/images/404.png' />
     </Box>
   );
